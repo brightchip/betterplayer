acb1316ca235300efc46b235e2b414704db1d2e2
mac <>
2025-01-15 12:45:13 +0800

    no message

--- android/src/main/kotlin/com/jhomlala/better_player/BetterPlayer.kt
+++ android/src/main/kotlin/com/jhomlala/better_player/BetterPlayer.kt
@@ -368,6 +368,7 @@ internal class BetterPlayer(
                 FORMAT_DASH -> C.CONTENT_TYPE_DASH
                 FORMAT_HLS -> C.CONTENT_TYPE_HLS
                 FORMAT_OTHER -> C.CONTENT_TYPE_OTHER
+                FORMAT_MP4 -> C.CONTENT_TYPE_OTHER
                 FORMAT_RTSP -> C.CONTENT_TYPE_RTSP
                 else -> -1
             }
@@ -734,6 +735,7 @@ internal class BetterPlayer(
         private const val FORMAT_HLS = "hls"
         private const val FORMAT_RTSP = "rtsp"
         private const val FORMAT_OTHER = "other"
+        private const val  FORMAT_MP4 = "mp4"
         private const val DEFAULT_NOTIFICATION_CHANNEL = "BETTER_PLAYER_NOTIFICATION"
         private const val NOTIFICATION_ID = 20772077
 
